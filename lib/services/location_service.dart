d05325a871c611ccb7203170ebf297ad729b4ba2
roqaia-ali <roqaiaali777@gmail.com>
2026-05-12 22:44:57 +0300

    second commit

--- lib/services/location_service.dart
+++ lib/services/location_service.dart
@@ -2,14 +2,18 @@ import 'package:geolocator/geolocator.dart';
 
 class LocationService {
   Future<bool> requestPermission() async {
-    bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
-    if (!serviceEnabled) return false;
-
     LocationPermission permission = await Geolocator.checkPermission();
+
     if (permission == LocationPermission.denied) {
       permission = await Geolocator.requestPermission();
-      if (permission == LocationPermission.denied) return false;
     }
-    return permission != LocationPermission.deniedForever;
+
+    if (permission == LocationPermission.deniedForever) {
+      await Geolocator.openAppSettings();
+      return false;
+    }
+
+    return permission == LocationPermission.whileInUse ||
+        permission == LocationPermission.always;
   }
 }
